ddb10bb9af80fcb30ae2c215d522ecd67d9213e0
audrey.huguenin <audrey.huguenin@heig-vd.ch>
2019-07-08 16:46:27 +0200

    add project, add subtask

--- public/js/planning.js
+++ public/js/planning.js
@@ -1,9 +1,8 @@
 
 var maxField = 100; //Input fields increment limitation
-var addButton = $('.add_task'); //Add button selector
-var addButtonSubtask = $('.add_subtask');
 var x = 1; //Compteur de tâches ajoutées
 var y = 1; //Initial field counter is 1
+var z = 1;
 
 
 //Action du clique sur le bouton d'ajout de tâche. Cela a comme effet d'ajouter un bloc complet de tâche comprenant nom de la tâche, commentaire et user.
@@ -59,15 +58,15 @@ $('body').on('click', '.remove_button',  function(e){
     return false;
 });
 
-
-$(addButtonSubtask).click(function(e){
+//Au click du bouton d'ajout de sous-tâche
+$('body').on('click', '.add_subtask', function(e){
     e.preventDefault();
     if(y < maxField){ 
         y++; 
         var fieldHTML =
-                    '<div class="subtask"><div class="row"><div class="col-3">'
+                    '<div class="subtask"><div class="row"><a class="remove_sub" style="margin-top:10px;"><i class="fas fa-times"></i></a><div class="col-3">'
                     +'<select name="subtask"><option value="placeholder">Sous-tâche</option></select></div>'
-                    +'<div class="col-9"><div class="tasks">'
+                    +'<div class="col-8"><div class="tasks">'
                     +'<div class="task">'
                     +'<div class="row">'
                     +'<button class="remove_button btn btn-danger col-1"><i class="fas fa-trash-alt"></i></button>'
@@ -80,9 +79,24 @@ $(addButtonSubtask).click(function(e){
                     +'</div></div>'
                     +'</div><button class="add_task btn btn-danger">+ Add new task</button></div></div></div>'
         
-        var blocTask = $(this).parent().children(2)[3];  
+        var blocTask = $(this).parent().find('.subtasks');  
         $(blocTask).append(fieldHTML); //Add field html
         //Increment field counter
+        var selectToFill= $(this).parent().find('select[name=subtask]').last();
+        var subtasks = $.get("/subtasks", {
+            "project_id": $(this).parent().find('input.projecttypeahead').val()
+        }, function (data) {
+            
+            selectToFill.find('option')
+                .remove()
+                .end();
+            data.forEach(function (e, i) {
+                selectToFill.append($('<option></option>')
+                    .val(e.id)
+                    .text(e.name)); 
+            });
+        });
+
 
     $('input.usertypeahead').each(function( index ) {
     $(this).typeahead({
@@ -102,10 +116,75 @@ $(addButtonSubtask).click(function(e){
     return false;
 });
     
-//Quand le bouton de suppression de la tâche est cliqué, supprime l'élément lié. 
- $(wrapper).on('click', '.remove_button', function(e)
-{        e.preventDefault();
-        $(this).parent('div').remove(); //Remove field html
-        x--; //Decrement field counter
+//Quand le bouton de suppression de la sous-tâche est cliqué, supprime l'élément lié. 
+ $('body').on('click', '.remove_sub ', function(e)
+{       e.preventDefault();
+        $(this).parent().parent('div').remove(); //Remove field html
+        y--; //Decrement field counter
+        console.log(y);
+        return false;
+});
+
+
+
+//Au click du bouton d'ajout de projet
+$('body').on('click', '.add_project', function(e){
+    e.preventDefault();
+    console.log("hola");
+     if(z < maxField){ 
+        z++; 
+        var fieldHTML =
+                    '<div class="project" style="border-bottom: 1px lightgray solid;">'
+                    +'<div class="row"><a class="remove_project" style="margin-top:10px;"><i class="fas fa-times"></i></a>'
+                    +'<div class="col-11">'  
+                    +'<input class="projecttypeahead" autocomplete="off" placeholder="Choisis le projet" name="project_typeahead" type="text" value=""></input>'  
+                    +'<input id="project_id" name="project" type="hidden" value="9990">'
+                    +'</div></div>'
+                    +'<div class="subtasks"></div>'
+                    +'<button class="add_subtask btn btn-danger">+ Add subtask</button></div></div>';
+        
+    console.log($(this).parent().children(3).children()[2]);          
+        var blocTask = $(this).parent().children(3).children()[2];  
+        $(blocTask).append(fieldHTML); //Add field html
+        //Increment field counter
+
+    $('input.projecttypeahead').each(function( index ) {
+        var $this = $(this);  
+    $(this).typeahead({
+    source:  function (query, process) {
+    return $.get('/projectautocomplete', { query: query }, function (data) {
+            return process(data);
+            });      
+    },
+    updater: function (obj) {
+        var subtasks = $.get("/subtasks", {
+            "project_id": obj.name
+        }, function (data) {
+            console.log(data);
+            $this.parent().parent().parent().find('select[name=subtask]').find('option')
+                .remove()
+                .end();
+            data.forEach(function (e, i) {
+                $this.parent().parent().parent().find('select[name=subtask]').append($('<option></option>')
+                    .val(e.id)
+                    .text(e.name));
+            });
+        });
+        $('.project_id').val(obj.id);
+        return obj;
+    }
+});
+});
+
+    }     
+    return false;
+});
+//Quand le bouton de suppression de la sous-tâche est cliqué, supprime l'élément lié. 
+$('body').on('click', '.remove_project ', function(e)
+{      
+        e.preventDefault();
+        $(this).parent().parent('div').remove(); //Remove field html
+        y--; //Decrement field counter
+        console.log(y);
         return false;
 });
--- resources/views/am/create.blade.php
+++ resources/views/am/create.blade.php
@@ -1,118 +1,126 @@
 <!DOCTYPE html>
 <html lang="en">
- <head>
-@include('layouts.partials.head')
 
- </head>
- <body>
- {{ Form::open(array('url' => 'plannings')) }}
-@include('layouts.partials.nav_am') 
+<head>
+    @include('layouts.partials.head')
 
-<button type="button" class="btn btn-danger btn-lg fixed-bottom addproject">+ Ajouter projet</button>
+</head>
+
+<body>
+    {{ Form::open(array('url' => 'plannings')) }}
+    @include('layouts.partials.nav_am')
+
+    <button type="button" class="btn btn-danger btn-lg fixed-bottom add_project">+ Ajouter projet</button>
 
 
-<div class="container">
-  
-    <div class="col-12">
-
-      <h2 class="title">Remplissage planning</h2>
-
-          
-          <!-- select box -->
-
-         
-          {{ Form::text('project_typeahead','', array('class'=>'projecttypeahead', 'autocomplete'=>"off", 'placeholder'=>'Choisis le projet')) }}
-          {{ Form::hidden('project','', array('id'=>'project_id')) }}       
-  
-<div class="subtasks">
-      <div class="subtask">
-          <div class="row">
-            <div class="col-3">   
-          {{Form::select('subtask', array('placeholder'=>'Sous-tâche'))}}
-            </div>
-            <div class="col-9">
-                <div  class="tasks">
-                    <div class="task">
+    <div class="container">
+        <h2 class="title">Remplissage planning</h2>
+        <div class="projects">
+            <div class="project" style="border-bottom: 1px lightgray solid;">
+                <div class="row">
+                    <a class="remove_project" style="margin-top:10px;"><i class="fas fa-times"></i></a>
+                    <div class="col-11">
+                        {{ Form::text('project_typeahead','', array('class'=>'projecttypeahead', 'autocomplete'=>"off", 'placeholder'=>'Choisis le projet')) }}
+                        {{ Form::hidden('project','', array('class'=>'project_id')) }}
+                    </div>
+                </div>
+                <div class="subtasks">
+                    <div class="subtask">
                         <div class="row">
-                          <button class="remove_button btn btn-danger col-1"><i class="fas fa-trash-alt"></i></button>
-                          <div class="row col-11">
-                             
-                                  {{ Form::text('task[task1][task_name]','',array('autocomplete'=>"off",'class'=>'', 'placeholder'=>'Nom de la tâche')) }}
-                                  {{ Form::text('task[task1][comment]','', array('class'=>'comment', 'autocomplete'=>"off", 'placeholder'=>'comment')) }}
-                             
-                                  {{ Form::text('user_typeahead','', array('class'=>'usertypeahead', 'autocomplete'=>"off", 'placeholder'=>'Qui qui doit faire?')) }}
-                                  {{ Form::hidden('task[task1][user]','', array('class'=>'user_id')) }}   
-                          </div>
+                            <a class="remove_sub" style="margin-top:10px;"><i class="fas fa-times"></i></a>
+                            <div class="col-3">
+                                {{Form::select('subtask', array('placeholder'=>'Sous-tâche'))}}
+                            </div>
+                            <div class="col-8">
+                                <div class="tasks">
+                                    <div class="task">
+                                        <div class="row">
+                                            <button class="remove_button btn btn-danger col-1"><i
+                                                    class="fas fa-trash-alt"></i></button>
+                                            <div class="row col-11">
+
+                                                {{ Form::text('task[task1][task_name]','',array('autocomplete'=>"off",'class'=>'', 'placeholder'=>'Nom de la tâche')) }}
+                                                {{ Form::text('task[task1][comment]','', array('class'=>'comment', 'autocomplete'=>"off", 'placeholder'=>'comment')) }}
+
+                                                {{ Form::text('user_typeahead','', array('class'=>'usertypeahead', 'autocomplete'=>"off", 'placeholder'=>'Qui qui doit faire?')) }}
+                                                {{ Form::hidden('task[task1][user]','', array('class'=>'user_id')) }}
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <button class="add_task btn btn-danger">+ Add new task</button>
+                            </div>
                         </div>
                     </div>
                 </div>
-                <button class="add_task btn btn-danger">+ Add new task</button>
+                <button class="add_subtask btn btn-danger">+ Add subtask</button>
+
+                {{ Form::close() }}
             </div>
-          </div>
-      </div>
-  </div>   
-  <button class="add_subtask btn btn-danger">+ Add subtask</button>
-            
-          {{ Form::close() }}
-</div>
+        </div>
 
-<div class="offset-1 col6"></div>
-
-</div>
+    </div>
     <script src="{{asset('/js/login.js')}}"></script>
     <script src="{{asset('/js/planning.js')}}"></script>
 
-   <script type="text/javascript">
+    <script type="text/javascript">
+        var pathproject = "{{ route('projectautocomplete') }}";
+        //console.log($('input.typeahead').typeahead('val'));
+        $('input.projecttypeahead').each(function(index){
+          var $this = $(this);  
+       $this.typeahead({
+            source: function (query, process) {
+                return $.get(pathproject, {
+                    query: query
+                }, function (data) {
+                    return process(data);
+                });
+            },
+            updater: function (obj) {
+                var subtasks = $.get("{{route('subtasks.index')}}", {
+                    "project_id": obj.name
+                }, function (data) {
+                    console.log(obj);
+                    $this.parent().parent().parent().find('select[name=subtask]').find('option')
+                        .remove()
+                        .end();
+                    data.forEach(function (e, i) {
+                      $this.parent().parent().parent().find('select[name=subtask]').append($('<option></option>')
+                            .val(e.id)
+                            .text(e.name));
+                    });
 
-    var pathproject = "{{ route('projectautocomplete') }}";
-    //console.log($('input.typeahead').typeahead('val'));
-    $('input.projecttypeahead').typeahead({
-    source:  function (query, process) {
-    return $.get(pathproject, { query: query }, function (data) {
-            return process(data);
+                });
+                $('.project_id').val(obj.id);
+                return obj;
+            },
+        })
+      });
+
+        var pathuser = "{{ route('userautocomplete') }}";
+        //console.log($('input.typeahead').typeahead('val'));
+        $('input.usertypeahead').each(function (index) {
+            $(this).typeahead({
+                source: function (query, process) {
+                    return $.get(pathuser, {
+                        query: query
+                    }, function (data) {
+                        return process(data);
+                    });
+
+                },
+                updater: function (obj) {
+                    $('.user_id').val(obj.id);
+                    return obj;
+                },
             });
-    },
-    updater: function(obj){
-      var subtasks = $.get("{{route('subtasks.index')}}", {"project_id":obj.name}, function(data){
-        console.log(data);
-        $('select[name=subtask]').find('option')
-                      .remove()
-                      .end();
-        data.forEach(function(e, i){
-          $('select[name=subtask]').append($('<option></option>')
-                      .val(e.id)
-                      .text(e.name)); 
         });
 
-      });
-      $('#project_id').val(obj.id);
-      return obj ;
-    },   
-});
-
-var pathuser = "{{ route('userautocomplete') }}";
-    //console.log($('input.typeahead').typeahead('val'));
-    $('input.usertypeahead').each(function( index ) {
-    $(this).typeahead({
-    source:  function (query, process) {
-    return $.get(pathuser, { query: query }, function (data) {
-            return process(data);
-            });
-            
-    },
-    updater: function(obj){  
-      $('.user_id').val(obj.id);
-      return obj ;
-    },   
-});
-});
-
-
-
-
-
-</script>
+    </script>
 
 
 </body>
+<footer class="footerAM">
+</footer>
+
 </html>
